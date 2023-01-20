6bd99fa72260720eec6496ba5ebc2d3292e5b85a
Jasmitha Damarla <snvjas3.d@gmail.com>
2023-01-20 13:19:14 +0530

    src of sampleproject maven

---
+++ src/main/java/com/sample/app/App.java
@@ -0,0 +1,14 @@
+package com.sample.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+	  System.out.println("I am App.java of SampleProject");
+    }
+}
